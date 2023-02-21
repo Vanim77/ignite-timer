ce5009c72d1cf93cbbba5e4f3bce50cd23fb9f1a
vanim <vanimouzad123@gmail.com>
2023-02-21 18:44:44 -0300

    :sparkles: feat: addNewCycleAction on actions.ts

---
+++ src/reducers/cycles/actions.ts
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-vars */
+// Disable because the integration with typescript have a bug with "no unused vars"
+
+import { ICycle } from './reducer'
+
+export enum ActionTypes {
+  ADD_NEW_CYCLE = 'ADD_NEW_CYCLE',
+  INTERRUPT_CURRENT_CYCLE = 'INTERRUPT_CURRENT_CYCLE',
+  MARK_CURRENT_CYCLE_AS_FINISHED = 'MARK_CURRENT_CYCLE_AS_FINISHED',
+}
+
+export function addNewCycleAction(newCycle: ICycle) {
+  return {
+    type: ActionTypes.ADD_NEW_CYCLE,
+    payload: {
+      newCycle,
+    },
+  }
+}
